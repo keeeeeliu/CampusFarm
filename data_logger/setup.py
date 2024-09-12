635decf26ee408a7f32661cc4415c0b5e283d903
nelfigs <nelfigs@umich.edu>
2024-09-12 19:24:47 -0400

    CF: adding the public py code to get data off datalogger

---
+++ data_logger/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+from hobo import __version__
+
+
+setup(
+    name='hobo',
+    version=__version__,
+    description='A simple module for reading Onset HOBO data logger data',
+    long_description=open('README.txt').read(),
+    url='https://github.com/nationalparkservice/hobo-py',
+    author='David A. Riggs',
+    author_email='david_a_riggs@nps.gov',
+    py_modules=['hobo'],
+    test_suite='test_hobo',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: Public Domain',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+    ],
+)
---
+++ data_logger/test_hobo.py
@@ -0,0 +1,53 @@
+
+import os.path
+import unittest
+
+import hobo
+
+
+class TestTimezones(unittest.TestCase):
+
+	def test_tz(self):
+		data = [
+			('GMT-08:00',  -8, 'U23 and H08 PST'),
+			('GMT+05:00',  +5, 'U23 and H08 Nepal'),
+			('GMT +05:00', +5, 'MX2301 Nepal'),
+			('GMT -08:00', -8, 'MX2301 PST')
+		]
+		for value, offset, desc in data:
+			tz = hobo.TZFixedOffset(value)
+			self.assertEqual(tz.offset_hrs, offset, desc)
+		for _, offset, desc in data:
+			tz = hobo.TZFixedOffset(offset)
+			self.assertEqual(tz.offset_hrs, offset, desc)
+
+
+class TestSampleData(unittest.TestCase):
+
+	def test_u23_hoboware(self):
+		fname = os.path.join('test_data', 'U23-001_HOBOware.csv')
+		with hobo.HoboCSVReader(fname) as reader:
+			self.assertEqual(reader.fname, fname)
+			self.assertEqual(reader.sn, '10173910')
+			#self.assertEqual(reader.title, 'Hobo U23-001 Sample Data')  # FIXME: remove BOM and "Plot Title:"
+			self.assertEqual(reader.tz, hobo.TZFixedOffset(-8))
+
+	def test_h08_hoboware(self):
+		fname = os.path.join('test_data', 'H08-030-08_HOBOware.csv')
+		with hobo.HoboCSVReader(fname) as reader:
+			self.assertEqual(reader.fname, fname)
+			self.assertEqual(reader.sn, '274341')
+			#self.assertEqual(reader.title, 'Hobo H08-030-08 Sample Data')
+			self.assertEqual(reader.tz, hobo.TZFixedOffset(-7))
+
+	def test_mx2301(self):
+		fname = os.path.join('test_data', 'MX2301_HOBOmobile.csv')
+		with hobo.HoboCSVReader(fname) as reader:
+			self.assertEqual(reader.fname, fname)
+			self.assertEqual(reader.sn, '20293298')
+			#self.assertEqual(reader.title, 'Hobo H08-030-08 Sample Data')
+			self.assertEqual(reader.tz, hobo.TZFixedOffset(+5))
+
+
+if __name__ == '__main__':
+	unittest.main()
