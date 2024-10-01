5ee3c703b0afdf4e406d97d1ea03113c56e57599
nelfigs <nelfigs@umich.edu>
2024-10-01 11:18:53 -0400

    CF: adding charger api connection script

--- charger/iq60_connection.py
+++ charger/iq60_connection.py
@@ -1,11 +1,14 @@
 import http.client
 
+
+
 conn = http.client.HTTPSConnection("api.enphaseenergy.com")
 payload = ''
 headers = {
-  'Authorization': 'Bearer unique_access_token'
+  'Authorization': '355f50fbae2cfdd2defa56de369d5d6c'
+  #if the auth code doesn't work, try this --> fcc3b392452b9b845a3a4cba80eece16
 }
-conn.request("GET", "/api/v4/systems?key=b2b2fd806ed13efb463691b436957798", payload, headers)
+conn.request("GET", "/api/v4/systems?key=fcc3b392452b9b845a3a4cba80eece16", payload, headers)
 res = conn.getresponse()
 data = res.read()
 print(data.decode("utf-8"))
