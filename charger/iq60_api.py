5a28877c9c3a1fd5d25c161f262f3cafa6a1c776
nelfigs <nelfigs@umich.edu>
2024-10-06 22:56:09 -0400

    CF: adding working charger api connection

---
+++ charger/iq60_api.py
@@ -0,0 +1,14 @@
+import http.client
+
+conn = http.client.HTTPSConnection("api.enphaseenergy.com")
+payload = ''
+headers = {
+    #insert access_token
+  'Authorization': 'Bearer eyJhbGciOiJSUzI1NiJ9.eyJhcHBfdHlwZSI6InN5c3RlbSIsInVzZXJfbmFtZSI6ImNhbXB1c2Zhcm1AdW1pY2guZWR1IiwiZW5sX2NpZCI6IiIsImVubF9wYXNzd29yZF9sYXN0X2NoYW5nZWQiOiIxNzI3MTkxNjkyIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImNsaWVudF9pZCI6IjNiNTRlZTExYTMwMzdhZDQzMmFmODhiM2NkYTJkOWRjIiwiYXVkIjpbIm9hdXRoMi1yZXNvdXJjZSJdLCJpc19pbnRlcm5hbF9hcHAiOmZhbHNlLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwiZXhwIjoxNzI4MzIyMjk4LCJlbmxfdWlkIjoiNDgxNTIxMSIsImFwcF9JZCI6IjE0MDk2MjQ5ODk1OTUiLCJqdGkiOiI1NzBhNDJhMy0yMGY1LTQ4NjMtOTBhYS02MjVhYzdiNGM1NWEifQ.BruSjAiWNqBUmZf6QLbmFZo3efu4mx5jG8KYy4JgQFPkudvTs1EwnfqG1og8_H1sfh3LIHfMrE_vu_Y9kjPb59lzx_tBU47gCCIACewym1gmspT7Wg08vHBfDpjg48juZhAmdKH8SO0dymFSqZtmtmU8MRbGCNRS01QPIsZj-4o'
+}
+
+conn.request("GET", "/api/v4/systems?key=d1128633c6e5948cd2c26be1c668900b", payload, headers)
+#/5458246/ev_charger/devices
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
