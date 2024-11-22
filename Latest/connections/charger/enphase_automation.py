01d4239656291816ef3e9dff07444e28b7455bc1
nelfigs <nelfigs@umich.edu>
2024-11-21 19:17:51 -0500

    deleting imports

--- Latest/connections/charger/enphase_automation.py
+++ Latest/connections/charger/enphase_automation.py
@@ -1,10 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import chromedriver_autoinstaller
 import undetected_chromedriver as uc
 import time
 
--- Latest/connections/ev_battery.py
+++ Latest/connections/ev_battery.py
@@ -1,9 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import chromedriver_autoinstaller
 import time
 import undetected_chromedriver as uc
 
