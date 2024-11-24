d93399e3a45404e7e51383032537f93e73b5cf7c
xinyixuu <113311231+xinyixuu@users.noreply.github.com>
2024-11-24 16:23:41 -0500

    Update webAPIinstall.sh

--- webAPI/bin/webAPIinstall.sh
+++ webAPI/bin/webAPIinstall.sh
@@ -5,11 +5,13 @@
 set -Eeuo pipefail
 set -x
 
+# Uncomment this command if no virtual environment
 # Create a Python virtual environment
-python3 -m venv env
+# python3 -m venv env
 
+# Uncomment this command if the virtual environment created is not activated
 # Activate Python virtual environment
-source env/bin/activate
+# source env/bin/activate
 
 # Install back end
 pip install -r requirements.txt
